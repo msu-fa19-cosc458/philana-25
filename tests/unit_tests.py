c4fcc3b7bbe6012559df48ef17da5e1302a32182
Ubuntu <ubuntu@ip-172-31-80-224.ec2.internal>
2019-10-16 14:33:17 +0000

    added a failing test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -12,7 +12,7 @@ class ChatBotResponseTest(unittest.TestCase):
         
     def test_divide_command(self):
         response = functions.get_chatbot_response("!! divide 2 2")
-        self.assertEquals(response, 1)
+        self.assertEquals(response, 5)
 
 if __name__ == '__main__':
     unittest.main()
